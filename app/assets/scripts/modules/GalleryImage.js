3d1b69d3d72447d60491a209d5e5abf7c2e207cc
RJ Van Hecke <vanheckeraoul@gmail.com>
2021-09-15 18:52:48 -0400

    styled seadek and created js for photo gallery

--- app/assets/scripts/modules/GalleryImage.js
+++ app/assets/scripts/modules/GalleryImage.js
@@ -1,12 +1,23 @@
 class GalleryImage {
   constructor() {
+    this.current = document.querySelector('#current')
+    this.thumbs = document.querySelectorAll(".pic-gallery__thumb")
 
     this.events()
   }
 
   events() {
-    console.log("image connected");
+    console.log('wtf')
+    console.log(this.current.src)
+    this.thumbs.forEach(this.changeSource);
+  }
+
+  changeSource(item) {
+    item.addEventListener('click', (e) => {
+      current.src = e.target.src
+    })
   }
 }
 
 export default GalleryImage
+
