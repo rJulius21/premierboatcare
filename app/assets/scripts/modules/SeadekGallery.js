6f1b8dd7660cb83e5fc481dbd8372c8aa68d6194
RJ Van Hecke <vanheckeraoul@gmail.com>
2021-09-29 11:25:18 -0400

    added basic js functionality to seadek services gallery

--- app/assets/scripts/modules/SeadekGallery.js
+++ app/assets/scripts/modules/SeadekGallery.js
@@ -1,15 +1,80 @@
 class SeadekGallery {
   constructor() {
+    this.leftArr = document.querySelector('.seadek-gallery__arrow--left')
+    this.rightArr = document.querySelector('.seadek-gallery__arrow--right')
     this.viewport = document.querySelector('#seadekViewport')
+    this.slider = document.querySelector('.seadek-gallery__slider')
+    this.imgBoxes = document.querySelectorAll('.seadek-gallery__img-box')
+    this.imgs = document.querySelectorAll('.seadek-gallery__img')
+
+    this.sliderCount = 0
+    this.sliderSize = getComputedStyle(this.slider).getPropertyValue("width")
 
     this.events()
   }
 
   events() {
-    console.log(this.viewport)
+    // console.log test
+    console.log(this.sliderSize)
+
+    // code for this module
+    this.scrollRight()
+    this.scrollLeft()
   }
 
+  // RIGHT ARROW FUNCTIONALITY
+  scrollRight() {
 
+    if (this.sliderCount < 2000) {
+      let isHeld = false;
+
+      this.rightArr.addEventListener('mousedown', () => {
+        isHeld = true;
+      });
+
+      this.rightArr.addEventListener('mouseup', () => {
+        isHeld = false;
+      });
+
+      setInterval(() => {
+        if (isHeld) {
+          // Code to loop goes here
+          this.sliderCount -= 2
+
+          console.log(this.sliderCount)
+          this.slider.style.left = `${this.sliderCount}px`;
+          return this.sliderCount
+        }
+      }, 0);
+    }
+
+  }
+  ////////////
+
+  // LEFT ARROW FUNCTIONALITY
+  scrollLeft() {
+    let isHeld = false;
+
+    this.leftArr.addEventListener('mousedown', () => {
+      isHeld = true;
+    });
+
+    this.leftArr.addEventListener('mouseup', () => {
+      isHeld = false;
+    });
+
+    setInterval(() => {
+      if (isHeld) {
+        // Code to loop goes here
+        this.sliderCount += 1
+
+        console.log(this.sliderCount)
+        this.slider.style.left = `${this.sliderCount}px`;
+        return this.sliderCount
+      }
+    }, 0);
+  }
 }
 
 export default SeadekGallery
+
